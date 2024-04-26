4ed943dddd06442b50f24f5812e2b0893110b3aa
refracta <refracta@abstr.net>
2024-04-27 06:18:57 +0900

    Minor fix

--- core/dwem-matcher-registry.js
+++ core/dwem-matcher-registry.js
@@ -12,22 +12,22 @@ export default class DWEMMatcherRegistry {
         };
     }
 
-    getModuleMatcher(targetModule) {
-        return ({module}) => module.endsWith(targetModule);
+    getModuleMatcher(regexp) {
+        return ({module}) => module.match(regexp);
     }
 
-    getURLMatcher(targetURL) {
-        return ({url}) => url.endsWith(targetURL);
+    getURLMatcher(regexp) {
+        return ({url}) => url.match(regexp);
     }
 
     constructor() {
         const mainModules = ['chat', 'client', 'comm'];
         for (const module of mainModules) {
-            this.matchers[module] = {'latest': this.getModuleMatcher(module)};
+            this.matchers[module] = {'latest': this.getModuleMatcher(new RegExp(`^${module}$`))};
         }
         const gameModules = ['./action_panel', './cell_renderer', './display', './dungeon_renderer', './enums', './game', './map_knowledge', './menu', './messages', './minimap', './monster_list', './mouse_control', './options', './player', './scroller', './text', './textinput', './tileinfo-dngn', './tileinfo-feat', './tileinfo-floor', './tileinfo-gui', './tileinfo-icons', './tileinfo-main', './tileinfo-player', './tileinfo-wall', './tileinfos', './ui-layouts', './ui', './util', './view_data'];
         for (const module of gameModules) {
-            this.matchers[module] = {'latest': this.getModuleMatcher(module.substring(1))};
+            this.matchers[module] = {'latest': this.getModuleMatcher(new RegExp(`${module.substring(1)}$`))};
         }
     }
 }
