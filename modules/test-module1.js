a4acf4d4da29a949545d0ce561e8c3c13868b5a3
refracta <refracta@abstr.net>
2024-04-28 15:13:13 +0900

    Add change minimap color example

--- modules/test-module1.js
+++ modules/test-module1.js
@@ -20,5 +20,23 @@ export default class TestModule1 {
 
         const myMapper2 = SMR.getSourceMapper('BeforeReturnInjection', `!${injectSource2.toString()}()`);
         SMR.add('chat', myMapper2);
+
+        function injectSource3() {
+            let original_get_option = get_option;
+            get_option = function (name) {
+                switch (name) {
+                    case 'tile_unseen_col':
+                        return {r: 255, g: 0, b: 0};
+                    case 'tile_floor_col':
+                        return {r: 0, g: 255, b: 0};
+                    case 'tile_wall_col':
+                        return {r: 0, g: 0, b: 255};
+                }
+                return original_get_option(name)
+            }
+        }
+
+        const myMapper3 = SMR.getSourceMapper('BeforeReturnInjection', `!${injectSource3.toString()}()`);
+        // SMR.add('./options', myMapper3);
     }
 }
