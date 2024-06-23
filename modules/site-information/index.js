55182debc3f53e408e85be81b28512e874f3754d
refracta <refracta@abstr.net>
2024-06-23 23:57:36 +0900

    Minor fix

--- modules/site-information/index.js
+++ modules/site-information/index.js
@@ -8,35 +8,36 @@ export default class SiteInformation {
         const {SourceMapperRegistry: SMR, MatcherRegistry: MR} = DWEM;
 
         function clientInjectSource() {
-            Object.defineProperty(DWEM.Modules.SiteInformation, 'current_user', {
+            const {SiteInformation} = DWEM.Modules;
+            Object.defineProperty(SiteInformation, 'current_user', {
                 get: function () {
                     return current_user;
                 },
                 enumerable: true,
                 configurable: true
             });
-            Object.defineProperty(DWEM.Modules.SiteInformation, 'watching', {
+            Object.defineProperty(SiteInformation, 'watching', {
                 get: function () {
                     return watching;
                 },
                 enumerable: true,
                 configurable: true
             });
-            Object.defineProperty(DWEM.Modules.SiteInformation, 'watching_username', {
+            Object.defineProperty(SiteInformation, 'watching_username', {
                 get: function () {
                     return watching_username;
                 },
                 enumerable: true,
                 configurable: true
             });
-            Object.defineProperty(DWEM.Modules.SiteInformation, 'playing', {
+            Object.defineProperty(SiteInformation, 'playing', {
                 get: function () {
                     return playing;
                 },
                 enumerable: true,
                 configurable: true
             });
-            Object.defineProperty(DWEM.Modules.SiteInformation, 'current_hash', {
+            Object.defineProperty(SiteInformation, 'current_hash', {
                 get: function () {
                     return current_hash;
                 },
