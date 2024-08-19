36a689d713bee98e813095b0b0191cf1e29bb86a
refracta <refracta@abstr.net>
2024-08-19 09:51:04 +0900

    Add CNCSplashScreen module

---
+++ modules/cnc-splash-screen/index.js
@@ -0,0 +1,11 @@
+export default class CNCSplashScreen {
+    static name = 'CNCSplashScreen';
+    static version = '0.1';
+    static dependencies = [];
+    static description = '(Beta) This module provides more splash screen images.';
+
+
+    onLoad() {
+
+    }
+}
