7cc531f2bd8b9407e10be60ed6b67ba47ed33c3c
refracta <refracta@abstr.net>
2024-08-22 03:06:36 +0900

    Minor fix

--- modules/cnc-chat/server/index.js
+++ modules/cnc-chat/server/index.js
@@ -18,12 +18,11 @@ app.use(express.json());
 
 const db = await JSONFilePreset('db.json', {entities: [], users: {}});
 
-// Ensure images directory exists
-fs.mkdirSync('images', {recursive: true});
+fs.mkdirSync('files', {recursive: true});
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-        cb(null, path.join(__dirname, 'images'));
+        cb(null, path.join(__dirname, 'files'));
     },
     filename: (req, file, cb) => {
         const ext = path.extname(file.originalname);
@@ -49,34 +48,34 @@ const upload = multer({
     fileFilter
 });
 
-app.post('/upload', upload.single('image'), async (req, res) => {
+app.post('/upload', upload.single('file'), async (req, res) => {
     try {
-        let {data} = req.body;
+        let {data: entity} = req.body;
         const {file} = req;
 
-        if (!data || !file) {
+        if (!entity || !file) {
             return res.status(400).json({error: 'Invalid request'});
         }
 
-        data = JSON.parse(data);
-        data.timestamp = Date.now();
-        data.ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
-        data.filename = `${data.user}.${data.timestamp}.png`;
-        const newFilePath = path.join(__dirname, 'images', data.filename);
-        data.filename = `images/${data.filename}`;
+        entity = JSON.parse(entity);
+        entity.timestamp = Date.now();
+        entity.ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
+        entity.file = `${entity.user}.${entity.timestamp}.png`;
+        const newFilePath = path.join(__dirname, 'files', entity.file);
+        entity.file = `files/${entity.file}`;
         fs.renameSync(file.path, newFilePath);
 
         let entityNumber;
         await db.update(({entities, users}) => {
-            entities.push(data);
+            entities.push(entity);
             entityNumber = entities.length;
-            if (!users[data.user]) {
-                users[data.user] = [];
+            if (!users[entity.user]) {
+                users[entity.user] = [];
             }
-            users[data.user].push(entityNumber);
+            users[entity.user].push(entityNumber);
         });
 
-        console.log('Uploaded data:', entityNumber, data);
+        console.log('Uploaded data:', entityNumber, entity);
         const url = `${config.entrypoint}/entities/${entityNumber}`;
         res.status(200).json({url});
     } catch (error) {
@@ -89,12 +88,12 @@ app.get('/entities/:entityNumber', async (req, res) => {
     try {
         const entityNumber = parseInt(req.params.entityNumber, 10);
         let entity = db.data.entities[entityNumber - 1];
-        entity = {...entity};
-        delete entity['ip'];
-        entity.image = `${config.entrypoint}/${entity.image}`;
         if (!entity) {
             return res.status(404).json({error: 'Entity not found'});
         }
+        entity = {...entity};
+        delete entity['ip'];
+        entity.file = `${config.entrypoint}/${entity.file}`;
         res.status(200).json(entity);
     } catch (error) {
         console.error(error);
@@ -102,7 +101,7 @@ app.get('/entities/:entityNumber', async (req, res) => {
     }
 });
 
-app.use('/images', express.static(path.join(__dirname, 'images')));
+app.use('/files', express.static(path.join(__dirname, 'files')));
 
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
