6c961c520737981cf6e4a8c4f2d5acf619227d3d
refracta <refracta@abstr.net>
2024-09-09 08:42:57 +0900

    Minor fix

--- modules/translation-module/index.js
+++ modules/translation-module/index.js
@@ -1,8 +1,29 @@
-export default class TranslationModule {
+class TranslationModule {
     static name = 'TranslationModule';
     static version = '0.1';
     static dependencies = ['IOHook'];
     static description = '(Beta) This module provides i18n feature.';
+
+    list = [
+        'a - a +0 hand axe (weapon)',
+        'c - a +0 short sword',
+        'b - a +0 animal skin',
+        'j - 4 scrolls of blinking',
+        'z - a wand of quicksilver (7)',
+        'B - the +7 great mace "Firestarter" {flame, immolate, rFlCloud, rF++}'
+    ];
+
+    matchers = [];
+
+    constructor() {
+        this.matchers = {
+            'INVENTORY_ITEM': {regex: /([a-zA-Z]) - (.+)/},
+            'A_ITEM': {},
+            'THE_ITEM': {},
+            'N_ITEM': {},
+            'ARTIFACT': {},
+        }
+    }
 
     // translation_language  = ko
 
@@ -10,3 +31,6 @@ export default class TranslationModule {
 
     }
 }
+
+const translation = new TranslationModule();
+console.log(TranslationModule.list)
