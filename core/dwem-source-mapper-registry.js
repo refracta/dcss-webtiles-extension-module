f44d711635b7b0f852366372c96ba48689eb49ca
refracta <refracta@abstr.net>
2024-09-10 06:43:27 +0900

    Minor fix

--- core/dwem-source-mapper-registry.js
+++ core/dwem-source-mapper-registry.js
@@ -1,13 +1,12 @@
-import esprima from 'https://cdn.skypack.dev/esprima';
-import estraverse from 'https://cdn.skypack.dev/estraverse';
-import escodegen from 'https://cdn.skypack.dev/escodegen';
+import * as acorn from 'https://cdn.skypack.dev/acorn';
+import * as astring from 'https://cdn.skypack.dev/astring';
 
 export default class DWEMSourceMapperRegistry {
     sourceMappers = {};
 
     add(matcherIdentifier, sourceMapper, priority = 0) {
         if (!Array.isArray(matcherIdentifier)) {
-            matcherIdentifier = [matcherIdentifier]
+            matcherIdentifier = [matcherIdentifier];
         }
         matcherIdentifier = Array.from(new Set(matcherIdentifier.map(i => i.trim()).map(i => {
             const [name, version] = [...i.split(':'), 'all'];
@@ -20,34 +19,54 @@ export default class DWEMSourceMapperRegistry {
     predefinedSourceMappers = {
         'BeforeReturnInjection': (injectSource) => {
             return (source) => {
-                const ast = esprima.parseScript(`(${source})`);
                 let topLevelFunction = null;
-                estraverse.traverse(ast, {
-                    enter: function (node) {
-                        if (node.type === 'FunctionDeclaration' || node.type === 'FunctionExpression') {
-                            if (!topLevelFunction) {
-                                topLevelFunction = node;
+                const ast = acorn.parse(`(${source})`, {ecmaVersion: 'latest'});
+
+                const stack = [...ast.body];
+                while (stack.length > 0) {
+                    const currentNode = stack.pop();
+
+                    if (currentNode.type === 'FunctionDeclaration' || currentNode.type === 'FunctionExpression' || currentNode.type === 'ArrowFunctionExpression') {
+                        topLevelFunction = currentNode;
+                        break;
+                    }
+
+                    if (currentNode.type === 'ExpressionStatement' && currentNode.expression) {
+                        stack.push(currentNode.expression);
+                    }
+
+                    if (currentNode.type === 'VariableDeclaration') {
+                        for (const declaration of currentNode.declarations) {
+                            if (declaration.init) {
+                                stack.push(declaration.init);
                             }
                         }
                     }
-                });
 
-                if (topLevelFunction) {
-                    const newExpression = esprima.parseScript(injectSource).body[0].expression;
-                    const lastIndex = topLevelFunction.body.body.length - 1;
-                    const lastStatement = topLevelFunction.body.body[lastIndex];
-                    if (lastStatement.type === 'ReturnStatement') {
-                        topLevelFunction.body.body.splice(lastIndex, 0, {
-                            type: 'ExpressionStatement', expression: newExpression
-                        });
-                    } else {
-                        topLevelFunction.body.body.push({
-                            type: 'ExpressionStatement', expression: newExpression
-                        });
+                    if (currentNode.body && Array.isArray(currentNode.body)) {
+                        for (let i = currentNode.body.length - 1; i >= 0; i--) {
+                            stack.push(currentNode.body[i]);
+                        }
                     }
                 }
 
-                return escodegen.generate(ast).slice(1, -2);
+                if (!topLevelFunction) {
+                    throw new Error('No top-level function found.');
+                }
+                const expression = acorn.parseExpressionAt(injectSource, 0, {ecmaVersion: 'latest'});
+                const body = topLevelFunction.body.body;
+                const lastIndex = body.length - 1;
+                const lastStatement = body[lastIndex];
+                if (lastStatement.type === 'ReturnStatement') {
+                    body.splice(lastIndex, 0, {
+                        type: 'ExpressionStatement', expression
+                    });
+                } else {
+                    body.push({
+                        type: 'ExpressionStatement', expression
+                    });
+                }
+                return astring.generate(ast).slice(1, -3);
             };
         }
     }
---
+++ modules/new-rc-manager/index.js
@@ -0,0 +1,72 @@
+export default class RCManager {
+    // USE LOCAL_SOCKET?
+    static name = 'RCManager'
+    static version = '1.0'
+    static dependencies = ['IOHook', 'WebSocketFactory']
+    static description = '(Library) This module provides features for creating custom RC trigger logic.'
+    handlers = []
+
+    addHandler(identifier, handler, priority = 0) {
+        this.handlers.push({identifier, handler, priority});
+        this.handlers.sort((h1, h2) => h2.priority - h1.priority);
+    }
+
+    removeHandler(identifier) {
+        for (let i = this.handlers.length - 1; i >= 0; i--) {
+            if (this.handlers[i].identifier === identifier) {
+                this.handlers.splice(i, 1);
+            }
+        }
+    }
+
+    get_rc(game_id) {
+        const {WebSocketFactory} = DWEM.Modules;
+        return new Promise(resolve => {
+            const socket = WebSocketFactory.create((data) => {
+                WebSocketFactory.handle_login_cookie(data);
+                if (data.msg === 'login_success') {
+                    socket.send(JSON.stringify({msg: 'get_rc', game_id}));
+                } else if (data.msg === 'rcfile_contents') {
+                    socket.close();
+                    resolve({contents: data.contents, game_id});
+                }
+            });
+            socket.onopen = () => {
+                WebSocketFactory.login(socket);
+            }
+        });
+    }
+
+    onLoad() {
+        const {IOHook} = DWEM.Modules;
+        this.loginReady = new Promise(resolve => {
+            this.loginReadyResolver = resolve;
+        })
+        IOHook.send_message.after.addHandler('rc-manager', async (msg, data) => {
+            if (msg === 'play') {
+                await this.loginReady;
+                const rc = await this.get_rc(data.game_id);
+                for (const {handler} of this.handlers) {
+                    try {
+                        handler(msg, rc);
+                    } catch (e) {
+                        console.error(e);
+                    }
+                }
+            }
+        });
+        IOHook.handle_message.after.addHandler('rc-manager', async (data) => {
+            if (data.msg === 'go_lobby') {
+                for (const {handler} of this.handlers) {
+                    try {
+                        handler(data.msg);
+                    } catch (e) {
+                        console.error(e);
+                    }
+                }
+            } else if (data.msg === 'login_cookie') {
+                this.loginReadyResolver();
+            }
+        });
+    }
+}
