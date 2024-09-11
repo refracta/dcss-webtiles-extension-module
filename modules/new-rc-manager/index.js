948ef67ae4def9eb0b86469b215d4587748e8498
refracta <refracta@abstr.net>
2024-09-11 21:51:18 +0900

    Minor fix

--- modules/new-rc-manager/index.js
+++ modules/new-rc-manager/index.js
@@ -1,72 +1,58 @@
-export default class RCManager {
-    // USE LOCAL_SOCKET?
-    static name = 'RCManager'
+export default class NewRCManager {
+    static name = 'NewRCManager'
     static version = '1.0'
     static dependencies = ['IOHook', 'WebSocketFactory']
     static description = '(Library) This module provides features for creating custom RC trigger logic.'
-    handlers = []
 
-    addHandler(identifier, handler, priority = 0) {
-        this.handlers.push({identifier, handler, priority});
-        this.handlers.sort((h1, h2) => h2.priority - h1.priority);
-    }
-
-    removeHandler(identifier) {
-        for (let i = this.handlers.length - 1; i >= 0; i--) {
-            if (this.handlers[i].identifier === identifier) {
-                this.handlers.splice(i, 1);
-            }
-        }
-    }
-
-    get_rc(game_id) {
-        const {WebSocketFactory} = DWEM.Modules;
-        return new Promise(resolve => {
-            const socket = WebSocketFactory.create((data) => {
-                WebSocketFactory.handle_login_cookie(data);
-                if (data.msg === 'login_success') {
-                    socket.send(JSON.stringify({msg: 'get_rc', game_id}));
-                } else if (data.msg === 'rcfile_contents') {
-                    socket.close();
-                    resolve({contents: data.contents, game_id});
-                }
-            });
-            socket.onopen = () => {
-                WebSocketFactory.login(socket);
-            }
-        });
-    }
 
     onLoad() {
         const {IOHook} = DWEM.Modules;
-        this.loginReady = new Promise(resolve => {
-            this.loginReadyResolver = resolve;
-        })
-        IOHook.send_message.after.addHandler('rc-manager', async (msg, data) => {
-            if (msg === 'play') {
-                await this.loginReady;
-                const rc = await this.get_rc(data.game_id);
-                for (const {handler} of this.handlers) {
-                    try {
-                        handler(msg, rc);
-                    } catch (e) {
-                        console.error(e);
-                    }
-                }
+        IOHook.send_message.before.addHandler('rc2-manager', (msg, data) => {
+            if (msg === 'play' || msg === 'watch') {
+                this.session = data;
             }
         });
-        IOHook.handle_message.after.addHandler('rc-manager', async (data) => {
-            if (data.msg === 'go_lobby') {
-                for (const {handler} of this.handlers) {
-                    try {
-                        handler(data.msg);
-                    } catch (e) {
-                        console.error(e);
-                    }
+
+        IOHook.handle_message.before.addHandler('rc2-manager', (data) => {
+            if (data.delayed) {
+                return;
+            }
+            if (this.session && data.msg === 'game_client') {
+                if (this.session.game_id) {
+                    socket.send(JSON.stringify({msg: 'get_rc', game_id: this.session.game_id}));
                 }
-            } else if (data.msg === 'login_cookie') {
-                this.loginReadyResolver();
+                this.queue = [data];
+            } else if (this.session && data.msg === 'rcfile_contents') {
+                console.log(data);
+                setTimeout(async _ => {
+                    const copyQueue = this.queue;
+                    this.queue = null;
+                    for (const d of copyQueue) {
+                        console.log('replay', d);
+                        IOHook.handle_message({...d, delayed: true});
+                        if (['game_client'].includes(d.msg)) {
+                            await new Promise(resolve => {
+                                require([`game-${d.version}/game`], (cv) => {
+                                    console.log(cv);
+                                    resolve(cv);
+                                });
+                            })
+                        }
+                    }
+                }, 100);
+                return true;
+            } else if (this.queue) {
+                const now = Date.now();
+                if (this.prev) {
+                    console.log(this.prev - now, data)
+                }
+                this.queue.push(data);
+                this.prev = now;
+                return true;
             }
         });
+        // Play > get_rc faster
+        // Watch > Estimate
+
     }
 }
