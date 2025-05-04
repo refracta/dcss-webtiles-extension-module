be5133578892ea5d48b576d3425bab3511e84574
refracta <refracta@abstr.net>
2025-05-05 03:56:32 +0900

    Minor fix

--- backends/translation-web/core/static/core/js/translationdata_dynamic.js
+++ backends/translation-web/core/static/core/js/translationdata_dynamic.js
@@ -1,4 +1,4 @@
-const commitHash = '1a8866b9';
+const commitHash = '47e1fcdc';
 const translatorPromise = import((`https://cdn.jsdelivr.net/gh/refracta/dcss-webtiles-extension-module@${commitHash}/modules/translation-module/translator.js`));
 const dataManagerPromise = import((`https://cdn.jsdelivr.net/gh/refracta/dcss-webtiles-extension-module@${commitHash}/modules/translation-module/data-manager.js`));
 const fetchPromise = fetch('/build/latest.json', {cache: "no-store"}).then((r) => r.json());
