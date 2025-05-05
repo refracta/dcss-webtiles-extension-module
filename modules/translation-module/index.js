62c481ccb1f7e4df719f38f82d445de00a4e6bf7
refracta <refracta@abstr.net>
2025-05-05 17:12:27 +0900

    Minor fix

--- modules/translation-module/index.js
+++ modules/translation-module/index.js
@@ -249,7 +249,7 @@ export default class TranslationModule {
                                     const translatedList = list.map((unitText) => this.translator.translate(unitText, this.config.translationLanguage, key));
                                     if (this.config.translationDebug) {
                                         for (let i = 0; i < list.length; i++) {
-                                            console.log(`%c<${key} [${i}]:ORIGINAL>%c\n${list[i]}\n%c<${key} [${i}]:TRANSLATED>%c\n${translatedList[i].translation}\n%c<${key} [${i}]:RESULT>%c\n${JSON.stringify(translatedList[i], null, 4)}\nhttps://translation.nemelex.cards/admin/core/matcher/add/?category=${encodeURIComponent(key)}&raw=${encodeURIComponent(list[i])}&priority=0`, 'font-weight: bold; color: red', '', 'font-weight: bold; color: blue', '', 'font-weight: bold; color: grey', '');
+                                            console.log(`%c<${key} [${i}]:ORIGINAL>%c\n${list[i]}\n%c<${key} [${i}]:TRANSLATED>%c\n${translatedList[i].translation}\n%c<${key} [${i}]:RESULT>%c\n%o\n${JSON.stringify(translatedList[i], null, 4)}\nhttps://translation.nemelex.cards/admin/core/matcher/add/?category=${encodeURIComponent(key)}&raw=${encodeURIComponent(list[i])}&priority=0`, 'font-weight: bold; color: red', '', 'font-weight: bold; color: blue', '', 'font-weight: bold; color: grey', '', translatedList[i]);
                                         }
                                     }
                                     restore(data, translatedList.map(result => result.translation));
